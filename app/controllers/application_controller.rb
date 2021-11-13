a478c7ef290ca3341ec1e9d003cd3a3849f8c5be
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-13 20:37:32 +0530

    Added support for username in devise ( with validations )

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :configure_devise_parameters, if: :devise_controller?
+
+    private
+  
+    def configure_devise_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username], except: [:email])         # No Need
+    end
 end
