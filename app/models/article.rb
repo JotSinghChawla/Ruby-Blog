648d221d2d8e11ee068a31edd363744304175051
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-13 16:58:14 +0530

    Added pagination support

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ApplicationRecord
 
+    self.per_page = 6
+
     has_many :comments, dependent: :destroy
 
     validates :title, presence: true, length: { minimum: 5 }    
     validates :body, presence: true
     validates :tag, presence: true
-    validates :image_link, presence: true
-            #    format: { with: /\A(https:\/\/.+)|(http:\/\/.+)\z/i, message: 'Must be a URL for GIF, JPG or PNG image.' }
+    validates :image_link, presence: true, format: { with: /\A(https:\/\/.+)|(http:\/\/.+)\z/i, message: 'Must be a URL for GIF, JPG or PNG image.' }
 end
