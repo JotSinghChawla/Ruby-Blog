07732ec1bdceef42e65d1f51bcce6f6591425a08
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-15 14:20:18 +0530

    User only delete their own comment & added more Styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
     
     before_action :authenticate_user!
 
+    before_action :check_comment_owner, only: [:destroy]
+
     def create
         # if request.post?
             @article = Article.find(params[:article_id])
@@ -22,6 +24,16 @@ class CommentsController < ApplicationController
     end
 
     private 
+
+    def check_comment_owner 
+        if user_signed_in?
+            current_article = Article.find(params[:id])
+            unless current_user.id == current_article.user_id
+              flash[:alert]='Access denied! You are not owner of this Comment'
+              redirect_to "/articles/#{current_article.id}"
+            end
+          end
+    end
 
     def comment_params
         params.require(:comment).permit(:user, :body)
